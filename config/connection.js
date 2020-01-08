3fef94238a1d84476258a78340159f040458e606
Matt DeMonaco <mdemonaco@gmail.com>
2020-01-08 01:02:35 -0800

    Placed seed values to insert into burgers_db. Set up config connection to MySQL database.

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8000,
+    user: "root",
+    password: "password",
+    database: "burgers__db"
+})
+
+connection.connect(function(err) {
+ if (err) {
+    console.log("An error occured" + err.stack)
+    return;
+ }
+ console.log("connected as " + connection.threadId)
+})
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double-Double", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy Sriracha burger", true);
